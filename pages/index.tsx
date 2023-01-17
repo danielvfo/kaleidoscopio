b2597dd72e60f14a4a572621f50b0b4b643148a1
Daniel <danielvfo@gmail.com>
2023-01-17 00:48:06 -0300

    add footer component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { Fragment } from 'react';
 import { Popover, Transition } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Footer } from '../components/common';
 
 const navigation = [
   { name: 'Blogs', href: '/blogs' },
@@ -108,17 +109,6 @@ const socialLinks = [
   }
 ];
 
-function Copyright() {
-  return (
-    <>
-      &copy;
-      <Link color="inherit" href="/">
-        XY
-      </Link>{' '}
-      {new Date().getFullYear()}
-    </>
-  );
-}
 
 const Home: NextPage = () => {
   return (
@@ -361,28 +351,7 @@ const Home: NextPage = () => {
           {/* Portfolio List Ends */}
         </div>
       </div>
-      <footer className="bg-gray-800">
-        <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-          <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
-            {navigation.map((item) => (
-              <div key={item.name} className="px-5 py-2">
-                <Link href={item.href} className="text-base text-gray-400 hover:text-gray-500">
-                  {item.name}
-                </Link>
-              </div>
-            ))}
-          </nav>
-          <div className="mt-8 flex justify-center space-x-6">
-            {socialLinks.map((item) => (
-              <a rel='noreferrer' target="_blank" key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
-                <span className="sr-only">{item.name}</span>
-                <item.icon className="h-6 w-6" aria-hidden="true" />
-              </a>
-            ))}
-          </div>
-          <p className="mt-8 text-center text-base text-gray-400"><Copyright />, All rights reserved.</p>
-        </div>
-      </footer>
+      <Footer></Footer>
     </>
   );
 }
